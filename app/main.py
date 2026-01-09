e1a106b4d7ef1361a2e8224da30e2b3d1ac7f769
Samuel <samuelpierrej@gmail.com>
2026-01-09 16:00:46 -0300

    refactor: using new updated lifespan

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.api.v1.api import api_router
-
+from sqlmodel import SQLModel
+from app.db.database import engine
+from contextlib import asynccontextmanager
 
 
 app = FastAPI(title="EduChain API")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    
+    yield
+
 
 app.include_router(api_router, prefix="/api/v1")
