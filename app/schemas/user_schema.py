1e14ac6d361ac3d1a8cd976c41f1235df1533780
Samuel <samuelpierrej@gmail.com>
2026-01-09 16:07:27 -0300

    refactor: remove unused error classes and clean up codebase

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -2,24 +2,13 @@ from pydantic import BaseModel, Field, EmailStr
 from app.enums.user_enum import UserRole
 
 class UserBase(BaseModel):
-    first_name: str = Field(min_length=2)
-    last_name: str = Field(min_length=2)
-    username: str = Field(min_length=2)
-    email: EmailStr
-    role: UserRole
+    first_name: str = Field(..., min_length=2)
+    last_name: str = Field(..., min_length=2)
+    username: str = Field(..., min_length=2)
+    email: EmailStr = Field(...,)
 
 class UserCreate(UserBase):
-    password: str = Field(min_length=8)
-
-class Student(UserCreate):
-    role: UserRole = UserRole.STUDENT
-
-class Teacher(UserCreate):
-    role: UserRole = UserRole.TEACHER
-
-class Admin(UserCreate):
-    role: UserRole = UserRole.ADMIN
-
+    password: str = Field(..., min_length=4)
 
 class UserPublic(UserBase):
     id: int
