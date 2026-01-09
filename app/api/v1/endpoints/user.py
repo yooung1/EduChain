ed3f7c5717bf60c12315f9b236a1edcf0a0365fd
Samuel <samuelpierrej@gmail.com>
2026-01-09 16:02:45 -0300

    refactor: ref new create teacher endpoint and change user_schema logic

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -1,18 +1,18 @@
 from fastapi import Depends, status, APIRouter
-from sqlalchemy.orm import Session
 from app.db.database import engine, get_db
 from app.models.user_model import User
 from app.schemas import user_schema
 from typing import List, Annotated
-from app.db.database import Base
-from app.core.security import get_password_hash
+from app.core.security import get_password_hash, CheckRole
 from app.errors.user_errors import (
     UsernameOrEmailExist
 )
-from sqlmodel import select
+from sqlmodel import select, Session, SQLModel
+from app.enums.user_enum import UserRole
 
 
-Base.metadata.create_all(bind=engine)
+
+SQLModel.metadata.create_all(engine)
 
 router = APIRouter(prefix="/users", tags=["Users"])
 db = Annotated[Session, Depends(get_db)]
@@ -24,7 +24,7 @@ def get_users(db: db) -> Session:
 
 
 @router.post("/create/student/", response_model=user_schema.UserPublic, status_code=status.HTTP_201_CREATED)
-def create_student(user: user_schema.Student, db: db) -> User:
+def create_student(user: user_schema.UserCreate, db: db) -> User:
     statement = select(User).where((User.email == user.email) | (User.username == user.username))
     db_user = db.exec(statement).first()
     
@@ -36,6 +36,7 @@ def create_student(user: user_schema.Student, db: db) -> User:
         last_name=user.last_name,
         username=user.username,
         email=user.email,
+        role=UserRole.STUDENT,
         hashed_password=get_password_hash(password=user.password), 
     )
 
@@ -47,10 +48,24 @@ def create_student(user: user_schema.Student, db: db) -> User:
 
 
 @router.post("/create/teacher", status_code=status.HTTP_201_CREATED, response_model=user_schema.UserPublic)
-def create_teacher(user: user_schema.Teacher, db: db):
+def create_teacher(user: user_schema.UserCreate, db: db, allowed_roles: User = Depends(CheckRole([UserRole.ADMIN]))):
     statement = select(User).where((User.email == user.email) | (User.username == user.username))
     db_user = db.exec(statement).first()
 
     if db_user:
         raise UsernameOrEmailExist()
 
+    new_user = User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        username=user.username,
+        email=user.email,
+        role=UserRole.TEACHER,
+        hashed_password=get_password_hash(password=user.password), 
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
