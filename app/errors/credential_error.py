1ed71598a1171da4cdaafd80a61a407c6bb1230a
Samuel <samuelpierrej@gmail.com>
2026-01-09 16:05:21 -0300

    feat: add custom exception for role validation

---
+++ app/errors/credential_error.py
@@ -0,0 +1,11 @@
+from app.errors.base_error import ErrorBase
+from fastapi import status
+
+class CredentialException(ErrorBase):
+    detail = "It's wat not possible to validate those credentials"
+    status_code = status.HTTP_401_UNAUTHORIZED
+
+
+class UserNotAllowed(ErrorBase):
+    detail = "This user has no permission to execute this process"
+    status_code = status.HTTP_403_FORBIDDEN
