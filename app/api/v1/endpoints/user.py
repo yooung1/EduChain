8fbeea5121e16c81663103d2273009b6f7b28ed7
Samuel <samuelpierrej@gmail.com>
2026-01-09 11:11:48 -0300

    refactor: using sqlmodel instead of alchemy

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -9,6 +9,7 @@ from app.core.security import get_password_hash
 from app.errors.user_errors import (
     UsernameOrEmailExist
 )
+from sqlmodel import select
 
 
 Base.metadata.create_all(bind=engine)
@@ -19,15 +20,13 @@ db = Annotated[Session, Depends(get_db)]
 
 @router.get("/", response_model=List[user_schema.UserPublic], status_code=status.HTTP_200_OK)
 def get_users(db: db) -> Session:
-    return db.query(User).all()
+    return db.exec(select(User)).all()
 
 
 @router.post("/create/student/", response_model=user_schema.UserPublic, status_code=status.HTTP_201_CREATED)
 def create_student(user: user_schema.Student, db: db) -> User:
-    db_user = db.query(User).filter(
-        (User.email == user.email) | (User.username == user.username)
-    ).first()
-    
+    statement = select(User).where((User.email == user.email) | (User.username == user.username))
+    db_user = db.exec(statement).first()
     
     if db_user:
         raise UsernameOrEmailExist()
@@ -49,9 +48,8 @@ def create_student(user: user_schema.Student, db: db) -> User:
 
 @router.post("/create/teacher", status_code=status.HTTP_201_CREATED, response_model=user_schema.UserPublic)
 def create_teacher(user: user_schema.Teacher, db: db):
-    db_user = db.query(User).filter(
-        (User.email == user.email) | (User.username == user.username)
-    ).first()
+    statement = select(User).where((User.email == user.email) | (User.username == user.username))
+    db_user = db.exec(statement).first()
 
     if db_user:
         raise UsernameOrEmailExist()
