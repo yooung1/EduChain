b7df552d378b7e5da0a69224538161271da9b96e
Samuel <samuelpierrej@gmail.com>
2026-01-09 16:04:22 -0300

    refactor: using sqlmodel intead of sqlachemy

--- app/db/database.py
+++ app/db/database.py
@@ -1,25 +1,17 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine, Session, SQLModel
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./educhain.db"
 
-
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
 )
 
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-Base = declarative_base()
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    with Session(engine) as session:
+        yield session
