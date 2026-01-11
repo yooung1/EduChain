baee77963dec19d58c90648eae8479709d1eb35b
Samuel Pierre <87836619+yooung1@users.noreply.github.com>
2026-01-11 12:53:16 -0300

    Uncomment User model definition in models.py

--- app/user/models.py
+++ app/user/models.py
@@ -7,26 +7,18 @@ from typing import Optional
 from app.models.user_model import User
 
 
-# class User(SQLModel, table=True):
-#     __tablename__ = "users"
+class User(SQLModel, table=True):
+     __tablename__ = "users"
 
-#     id: Optional[int] = Field(default=None, primary_key=True, index=True)
+     id: Optional[int] = Field(default=None, primary_key=True, index=True)
     
-#     first_name: str = Field(nullable=False)
-#     last_name: str = Field(nullable=False)
-#     username: str = Field(unique=True, nullable=False)
-#     email: str = Field(unique=True, index=True, nullable=False)
-#     hashed_password: str = Field(nullable=False, min_length=4)
+     first_name: str = Field(nullable=False)
+     last_name: str = Field(nullable=False)
+     username: str = Field(unique=True, nullable=False)
+     email: str = Field(unique=True, index=True, nullable=False)
+     hashed_password: str = Field(nullable=False, min_length=4)
     
-#     role: UserRole = Field(
-#         sa_column=Column(SqlEnum(UserRole), nullable=False)
-#     )
-
-
-
-
-
-
-
-
+     role: UserRole = Field(
+         sa_column=Column(SqlEnum(UserRole), nullable=False)
+     )
 
