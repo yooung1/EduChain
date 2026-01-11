71c7461242aa5c55e985c0daa7d076014a7e2f32
Samuel Pierre <87836619+yooung1@users.noreply.github.com>
2026-01-11 12:47:46 -0300

    Delete commit_new_user function
    Removed the commit_new_user function from models.py.

--- app/user/models.py
+++ app/user/models.py
@@ -25,22 +25,6 @@ from app.models.user_model import User
 
 
 
-def commit_new_user(user_role: UserRole, user: User, db: Session) -> User:
-    new_user = User(
-        first_name=user.first_name,
-        last_name=user.last_name,
-        username=user.username,
-        email=user.email,
-        role=user_role,
-        hashed_password=get_password_hash(password=user.password), 
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
 
 
 
