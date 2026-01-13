18047e951423db99c600c9c3765249f7515a8517
Samuel <samuelpierrej@gmail.com>
2026-01-13 16:16:51 -0300

    refactor: add new constants files in the test folder

---
+++ tests/constants.py
@@ -0,0 +1,20 @@
+# Constantes para Admin
+ADMIN_FIRST_NAME = "super_admin"
+ADMIN_LASTNAME = "super_admin"
+ADMIN_USERNAME = "super_admin"
+ADMIN_EMAIL = "super_admin@gmail.com"
+ADMIN_PASSWORD = "senha_super_admin"
+
+# Constantes para Student
+STUDENT_FIRST_NAME = "student"
+STUDENT_LASTNAME = "student"
+STUDENT_USERNAME = "student"
+STUDENT_EMAIL = "student@gmail.com"
+STUDENT_PASSWORD = "senha_student"
+
+# Constantes para Teacher
+TEACHER_FIRST_NAME = "teacher"
+TEACHER_LASTNAME = "teacher"
+TEACHER_USERNAME = "teacher"
+TEACHER_EMAIL = "teacher@gmail.com"
+TEACHER_PASSWORD = "senha_teacher"
