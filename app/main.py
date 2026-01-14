1cf304cfd5e9bc4828f87611b8bdcafebbcf4f66
Samuel <samuelpierrej@gmail.com>
2026-01-14 14:22:49 -0300

    feat: new columns for the data base

--- app/main.py
+++ app/main.py
@@ -3,15 +3,17 @@ from app.global_endpoints_assembly import api_router
 from sqlmodel import SQLModel
 from app.db.database import engine
 from contextlib import asynccontextmanager
+from app.course.models import Course
+from app.klass.models import Klass
 
-
-app = FastAPI(title="EduChain API")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     SQLModel.metadata.create_all(engine)
     
     yield
+
+app = FastAPI(title="EduChain API", lifespan=lifespan)
 
 
 app.include_router(api_router, prefix="/api/v1")
