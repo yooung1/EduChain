56f60781a2bcaca698fb146b2c86457c2283b999
Samuel <samuelpierrej@gmail.com>
2026-01-14 14:22:08 -0300

    feature: new endpoint included

--- app/global_endpoints_assembly.py
+++ app/global_endpoints_assembly.py
@@ -1,5 +1,6 @@
 from app.auth.router import auth_router
 from app.user.router import user_router
+from app.course.router import course_router
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -8,3 +9,4 @@ api_router = APIRouter()
 
 api_router.include_router(user_router)
 api_router.include_router(auth_router)
+api_router.include_router(course_router)
