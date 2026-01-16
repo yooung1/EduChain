3b92dde926119af2ad10cd7f4e29bdc55b0d4177
Samuel <samuelpierrej@gmail.com>
2026-01-16 11:05:25 -0300

    feat: new klass and course endpoint

--- app/global_endpoints_assembly.py
+++ app/global_endpoints_assembly.py
@@ -1,6 +1,7 @@
 from app.auth.router import auth_router
 from app.user.router import user_router
 from app.course.router import course_router
+from app.klass.router import klass_router
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -10,3 +11,4 @@ api_router = APIRouter()
 api_router.include_router(user_router)
 api_router.include_router(auth_router)
 api_router.include_router(course_router)
+api_router.include_router(klass_router)
