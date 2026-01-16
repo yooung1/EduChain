4e68ba799c3d7ffb8766e69dad7d669b940403e3
Samuel <samuelpierrej@gmail.com>
2026-01-16 11:07:37 -0300

    refactor: separate db calls

--- app/course/router.py
+++ app/course/router.py
@@ -1,27 +1,30 @@
 from fastapi import APIRouter, status, Depends
-from app.course.schemas import CourseSchema
+from app.course.schemas import CourseSchemaPost, CourseSchemaPublic
 from app.course.models import Course
 from typing import Annotated, List
 from app.db.database import get_db
 from sqlmodel import Session, select
+from sqlalchemy.orm import selectinload
 from app.auth.service import CheckRole
 from app.user.router import UserRole
 from app.course.service import commit_new_course
 from app.errors.course_errors import CourseDoentsExist
 
 
-course_router = APIRouter(prefix="/course")
+course_router = APIRouter(prefix="/course", tags=["Course"])
 
 db = Annotated[Session, Depends(get_db)]
 
 
-@course_router.get("/", response_model=List[CourseSchema], status_code=status.HTTP_200_OK)
+@course_router.get("/", response_model=List[CourseSchemaPublic], status_code=status.HTTP_200_OK)
 def get_courses(db: db, allowed_roles: UserRole = Depends(CheckRole([UserRole.ADMIN, UserRole.TEACHER]))) -> Session:
-    return db.exec(select(Course)).all()
+    statement = select(Course).options(selectinload(Course.klasses))
+    
+    return db.exec(statement).all()
 
 
-@course_router.post("/create", status_code=status.HTTP_201_CREATED, response_model=CourseSchema)
-def create_course(db: db, course_data: CourseSchema, allowed_roles: UserRole = Depends(CheckRole([UserRole.TEACHER]))) -> Course:
+@course_router.post("/create", status_code=status.HTTP_201_CREATED, response_model=CourseSchemaPost)
+def create_course(db: db, course_data: CourseSchemaPost, allowed_roles: UserRole = Depends(CheckRole([UserRole.TEACHER]))) -> Course:
     new_course = commit_new_course(course_data=course_data, db=db)
 
     return new_course
