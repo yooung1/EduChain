540fc34fd36f231da2b62688bdb17d1489667529
Samuel <samuelpierrej@gmail.com>
2026-01-16 11:09:13 -0300

    feat: new klass endpoint

---
+++ app/klass/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, status, Depends
+from app.klass.schemas import KlassSchemaPublic, KlassSchemaPost
+from app.auth.service import CheckRole
+from app.user.service import UserRole
+from typing import Annotated, List
+from sqlmodel import Session, select
+from app.db.database import get_db
+from app.klass.models import Klass
+from app.klass.service import create_new_klass
+
+
+klass_router = APIRouter(prefix="/klass", tags=["Klass"])
+db = Annotated[Session, Depends(get_db)]
+
+
+@klass_router.get("/", status_code=status.HTTP_202_ACCEPTED, response_model=List[KlassSchemaPublic])
+def get_klasses(db: db,  allowed_rolles: UserRole = Depends(CheckRole([UserRole.ADMIN, UserRole.TEACHER]))) -> Session:
+    statement = select(Klass)
+    return db.exec(statement).all()
+
+
+@klass_router.post("/create/klass", status_code=status.HTTP_201_CREATED, response_model=KlassSchemaPublic)
+def create_klass(klass: KlassSchemaPost, db: db, allowed_rolles: UserRole = Depends(CheckRole([UserRole.ADMIN, UserRole.TEACHER]))):
+    return create_new_klass(db=db, new_klass=klass)
+
+    
