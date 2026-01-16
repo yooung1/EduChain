a6353450552f0dba140816eddaa5fad6602a2772
Samuel <samuelpierrej@gmail.com>
2026-01-16 11:09:37 -0300

    refactor: new schema public

--- app/klass/schemas.py
+++ app/klass/schemas.py
@@ -2,8 +2,16 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 
-class KlassSchema(BaseModel):
-    name: str = Field(min_length=5, nullable=False)
-    video: str = Field(min_length=5, nullable=False)
-    description: str = Field(min_length=10, nullable=False)
+class KlassSchemaPost(BaseModel):
+    id: Optional[int] = None
+    name: str = Field(min_length=5)
+    video: str = Field(min_length=5)
+    description: str = Field(min_length=10)
     course_id: Optional[int]
+
+
+class KlassSchemaPublic(KlassSchemaPost):
+    id: Optional[int] = None
+    name: str = Field(min_length=5)
+    video: str = Field(min_length=5)
+    description: str = Field(min_length=10)
