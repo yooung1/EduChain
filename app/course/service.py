4634c1c4ffbdbe42f7ceb79296e4be244e043553
Samuel <samuelpierrej@gmail.com>
2026-01-16 11:08:37 -0300

    chore: new function name

--- app/course/service.py
+++ app/course/service.py
@@ -1,9 +1,9 @@
 from app.course.models import Course
-from app.course.schemas import CourseSchema
+from app.course.schemas import CourseSchemaPost
 from sqlmodel import Session
 
 
-def commit_new_course(course_data:CourseSchema, db: Session) -> Course:
+def commit_new_course(course_data:CourseSchemaPost, db: Session) -> Course:
     new_course = Course(
         name=course_data.name,
         description=course_data.description
