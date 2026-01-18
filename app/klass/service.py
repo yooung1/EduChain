bffe36cfb5922492e191a87940ed8a56f1eaf4cc
Samuel <samuelpierrej@gmail.com>
2026-01-17 22:46:05 -0300

    refactor: db commit model handle list instead of single data

--- app/klass/service.py
+++ app/klass/service.py
@@ -1,18 +1,16 @@
 from app.klass.schemas import KlassSchemaPost
 from sqlmodel import Session
 from app.klass.models import Klass
+from typing import List
 
-def create_new_klass(db: Session, new_klass: KlassSchemaPost):
-    new_klass = Klass(
-        name=new_klass.name,
-        video=new_klass.video,
-        description=new_klass.description,
-        course_id=new_klass.course_id
-    )
+def create_new_klass(db: Session, new_klass: List[KlassSchemaPost]):
+    new_klasses_models = [Klass(**item.model_dump()) for item in new_klass]
 
+    db.add_all(new_klasses_models)
 
-    db.add(new_klass)
     db.commit()
-    db.refresh(new_klass)
 
-    return new_klass
+    for k in new_klasses_models:
+        db.refresh(k)
+
+    return new_klasses_models
