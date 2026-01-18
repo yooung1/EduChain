ce9076b288e735596c6c7fd403d9e804e32cd3d6
Samuel <samuelpierrej@gmail.com>
2026-01-17 22:15:02 -0300

    feat: new delete endpoint

--- app/klass/router.py
+++ app/klass/router.py
@@ -7,6 +7,7 @@ from sqlmodel import Session, select
 from app.db.database import get_db
 from app.klass.models import Klass
 from app.klass.service import create_new_klass
+from app.klass.exceptions import KlassDoesNotExist
 
 
 klass_router = APIRouter(prefix="/klass", tags=["Klass"])
@@ -19,8 +20,20 @@ def get_klasses(db: db,  allowed_rolles: UserRole = Depends(CheckRole([UserRole.
     return db.exec(statement).all()
 
 
-@klass_router.post("/create/klass", status_code=status.HTTP_201_CREATED, response_model=KlassSchemaPublic)
+@klass_router.post("/create", status_code=status.HTTP_201_CREATED, response_model=KlassSchemaPublic)
 def create_klass(klass: KlassSchemaPost, db: db, allowed_rolles: UserRole = Depends(CheckRole([UserRole.ADMIN, UserRole.TEACHER]))):
     return create_new_klass(db=db, new_klass=klass)
 
+
+
+@klass_router.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_klass(db: db, id: int, allowed_roles: UserRole = Depends(CheckRole([UserRole.ADMIN, UserRole.TEACHER]))):
+    klass = db.get(Klass, id)
     
+    if not klass:
+        raise KlassDoesNotExist
+    
+    db.delete(klass)
+    db.commit()
+
+    return None
