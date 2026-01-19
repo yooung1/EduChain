1e791fe0e3a3dba153899f9b40beb72980e65cb7
Samuel <samuelpierrej@gmail.com>
2026-01-19 11:27:31 -0300

    test: new tests for the delete and pacth endpoint

--- tests/user/test_user.py
+++ tests/user/test_user.py
@@ -220,3 +220,148 @@ def test_create_teacher_with_no_authorization(client, login_as_student, login_as
 
     response = client.post(url=URL, json=payload)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_delete_user(client, login_as_admin):
+    headers = {"Authorization": f"Bearer {login_as_admin}"}
+
+    URL = "/api/v1/users/create/student"
+    DELETE_URL = "/api/v1/users/delete/"
+    first_name = "student"
+    last_name = "student"
+    username = "student_222"
+    email = "student@example.com"
+    password = "senha12312312334"
+
+    payload = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "email": email,
+        "password": password
+    }
+
+    response = client.post(url=URL, json=payload, headers=headers)
+    assert response.status_code == status.HTTP_201_CREATED
+    data = response.json()
+
+    user_id = str(data["id"])
+
+    # delete user
+    response = client.delete(url=DELETE_URL+user_id, headers=headers)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_delete_user_with_no_authorization(client, login_as_student, login_as_admin, login_as_teacher):
+    student_auth = {"Authorization": f"Bearer {login_as_student}"}
+    teacher_auth = {"Authorization": f"Bearer {login_as_teacher}"}
+    admin_auth = {"Authorization": f"Bearer {login_as_admin}"}
+
+    URL = "/api/v1/users/create/student"
+    DELETE_URL = "/api/v1/users/delete/"
+    first_name = "student"
+    last_name = "student"
+    username = "student_222"
+    email = "student@example.com"
+    password = "senha12312312334"
+
+    payload = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "email": email,
+        "password": password
+    }
+
+    response = client.post(url=URL, json=payload, headers=admin_auth)
+    assert response.status_code == status.HTTP_201_CREATED
+    data = response.json()
+    user_id = str(data["id"])
+
+    # delete user as a student
+    response = client.delete(url=DELETE_URL+user_id, headers=student_auth)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+    # delete user as a teacher
+    response = client.delete(url=DELETE_URL+user_id, headers=teacher_auth)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+
+def test_patch_user(client, login_as_admin):
+    headers = {"Authorization": f"Bearer {login_as_admin}"}
+
+    URL = "/api/v1/users/create/student"
+    PATCH_URL = "/api/v1/users/edit/"
+    first_name = "student"
+    last_name = "student"
+    username = "student_222"
+    email = "student@example.com"
+    password = "senha12312312334"
+
+    payload = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "email": email,
+        "password": password
+    }
+
+    response = client.post(url=URL, json=payload, headers=headers)
+    assert response.status_code == status.HTTP_201_CREATED
+    user_id = str(response.json()["id"])
+
+    # edit data
+    new_username = "new_username1001"
+    new_payload = {
+        "username": new_username
+    }
+
+    response = client.patch(url=PATCH_URL+user_id, headers=headers, json=new_payload)
+    
+    assert response.status_code == status.HTTP_202_ACCEPTED
+
+    data = response.json()
+
+    assert data["username"] == new_username
+
+
+def test_patch_user_with_no_authorization(client, login_as_admin, login_as_student):
+    admin_auth = {"Authorization": f"Bearer {login_as_admin}"}
+    student_auth = {"Authorization": f"Bearer {login_as_student}"}
+
+    URL = "/api/v1/users/create/student"
+    PATCH_URL = "/api/v1/users/edit/"
+    first_name = "student"
+    last_name = "student"
+    username = "student_222"
+    email = "student@example.com"
+    password = "senha12312312334"
+
+    payload = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "email": email,
+        "password": password
+    }
+
+    response = client.post(url=URL, json=payload, headers=admin_auth)
+    assert response.status_code == status.HTTP_201_CREATED
+    user_id = str(response.json()["id"])
+
+    
+    new_username = "new_username1001"
+    new_payload = {
+        "username": new_username
+    }
+
+    # edit data as a student
+    response = client.patch(url=PATCH_URL+user_id, headers=student_auth, json=new_payload)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+    # edit data with no login
+    response = client.patch(url=PATCH_URL+user_id, json=new_payload)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
