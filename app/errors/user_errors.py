e8d8887d51e54d3ed4d9d7d8cba44fc4afd7a68e
Samuel <samuelpierrej@gmail.com>
2026-01-19 11:26:22 -0300

    feat: new error message class

--- app/errors/user_errors.py
+++ app/errors/user_errors.py
@@ -6,3 +6,7 @@ class UsernameOrEmailExist(ErrorBase):
     detail = "The username or email already exists"
     status_code = status.HTTP_409_CONFLICT
 
+
+class UserDoesNotExist(ErrorBase):
+    detail = "This User doesnt exist"
+    status_code = status.HTTP_404_NOT_FOUND
